a5a1ec58248501468310183a75a9e691b67f1692
Farok-Hossain <farokafs7@gmail.comm>
2023-09-26 23:52:43 +0600

    setup express mongoose cors parser

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+import cors from "cors";
+const port = 3000;
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working Successfully");
+});
+
+export default app;
+
+// JRmfvujCaav2QQcb
