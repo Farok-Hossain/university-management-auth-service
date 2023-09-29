f4ec1bc8373daf17f617a99fb94bc6e2d53a137e
Farok-Hossain <farokafs7@gmail.comm>
2023-09-29 22:05:34 +0600

    model interface

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
-import express, { Application, Request, Response } from "express";
-const app: Application = express();
-import cors from "cors";
-const port = 3000;
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from 'cors'
 
-app.use(cors());
+app.use(cors())
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working Successfully");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working Successfully')
+})
 
-export default app;
+export default app
 
 // JRmfvujCaav2QQcb
